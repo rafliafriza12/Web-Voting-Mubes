700fadcf2141de772ceaede268eb873745cb26d1
rafliafriza12 <rafliafriza90@gmail.com>
2023-12-04 14:54:30 +0700

    Template Laravel and Add font Poppins

---
+++ Web-Voting/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      }
+  },
+  },
+  plugins: [],
+}
